6d1d5583fe98d063ddc06dfa43f5b48ba7584284
Suellen Xavier <sxcwork@gmail.com>
2022-11-19 19:41:41 -0300

    update key active

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var keyboard = document.querySelectorAll(`input[type=button]`)
 for(index = 0; index < keyboard.length; index++){
     const key = keyboard[index]
     key.onclick = () =>{
-        inputPhone.value = inputPhone.value + key.value
+        inputPhone.value += key.value
+    }
+    key.onkeydown = function(evento){
+        if(evento.code === "Enter" || evento.code === "Space"){
+            key.classList.add('ativa')
+        }
+        key.onkeyup = ()=>{
+            key.classList.remove('ativa')
+        }
     }
 }
